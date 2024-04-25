6da8e9ffb6677efc0b6dfc1cc71314739434757f
sponge254 <g.stuk@outlook.com>
2024-04-25 08:03:37 -0400

    oops

--- frcwr.py
+++ frcwr.py
@@ -10,7 +10,8 @@ try:
 
     def getTBAData(url):
         result = requests.get(baseUrl + url, headers=header)
-        if result.status_code != 200 or list(result.json())[0] == "Error":
+        if result.status_code != 200 or len(result.json()) == 1:
+            print("--API Error-- Status code: " + str(result.status_code) + ", Error message: [" + result.json()['Error'] + ']')
             exit(404)
         return result.json()
 
