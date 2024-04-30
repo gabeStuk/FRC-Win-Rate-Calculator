42317ae70621586306176b1c35be70f82ad10cf2
sponge2 <117952984+gabeStuk@users.noreply.github.com>
2024-04-29 20:49:32 -0400

    CLI support run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
+@echo off
+set argc=0
+for %%x in (%*) do Set /A argc+=1
 python -m ensurepip --upgrade
 pip install requests
-python frcwr.py
+if argc GTR 0 (python frcwr.py %*) ELSE (python frcwr.py)
